3af65a5ab1d52ac9bc5f6bf0c9e3e85b484af01f
Maurice Boothe Jr <boothemjr@users.noreply.github.com>
2021-02-01 20:52:44 -0500

    add unity project

---
+++ ExampleProject/Assets/Scripts/WASDControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASDControl : MonoBehaviour
+{
+    public float forceAmt = 10;
+
+    private Rigidbody rb; // connects physics to an object in Unity
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(Vector3.forward * forceAmt);
+        }
+    }
+}
